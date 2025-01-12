1272e70ed52527402796baf0b8a134dc9f1e3ce1
Bruno Castro <86987726+CastroBrunno@users.noreply.github.com>
2025-01-12 18:56:48 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 
-
-//Realizar uma documentação clara para cada função
-
 import Empregados.ListaEmpregados;
 
 public class Main {
 
     public static void main(String[] args) {
+        //Dados ilustrativos, usados apenas para exemplo.
+        
         ListaEmpregados listaEmpregados = new ListaEmpregados();
         listaEmpregados.adicionarEmpregado("Bruno", 3.600, "Desenvolvedor Front-End Jr"); 
-        // listaEmpregados.exibirEmpregados();
-        // System.out.println("O numero de funcionarios é igual a: " + listaEmpregados.numeroDeEmpregados());
+        listaEmpregados.exibirEmpregados();
+        System.out.println("O numero de funcionarios é igual a: " + listaEmpregados.numeroDeEmpregados());
 
         listaEmpregados.adicionarEmpregado("Carlos", 1.500, "Desenvolvedor Back-End PL");
         listaEmpregados.adicionarEmpregado("joao", 1.600, "Desenvolvedor Fullstack Sr");
